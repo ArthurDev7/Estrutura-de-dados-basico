f0941b816d30158a910a9c1b3227db58e0cb9d4e
ArthurDev7 <arthurluizdev7@gmail.com>
2021-07-18 02:44:43 -0300

    Primeiro commit para estudo de estrutura de dados

---
+++ src/EstruturaDados/meuObj.java
@@ -0,0 +1,19 @@
+package EstruturaDados;
+
+public class meuObj {
+
+    Integer num;
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public meuObj(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return this.num.toString();
+    }
+}
